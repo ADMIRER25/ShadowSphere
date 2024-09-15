36898bd64b0b5b4033f84c97e371a4e0b4f07ef6
Subha Nayak <subhanayak022@gmail.com>
2024-09-15 20:53:15 +0530

    client a,b,c , client a and b chatting with each other and now if client c msg a then a can see msg of c in the middle of chatting with b along with b's message that means if multiple user msg one user at a time then user is unable to see which msg is send by whom he will only able to see that that all message is send by the user with which he is chatting but that is not multiple user sent msg to one user and all those message are placed in a single window I have solved the problem I haveidentified that selectedused id and new message which is coming via socket that's sender id should be same then socket can modify the current message array otherwise not problem solved

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -1,20 +1,24 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSocketContext } from "../context/SocketContext";
 import useConversation from "../zustand/useConversation";
 import notificationSound from '../assets/sounds/notification.mp3';
 
 const useListenMessage = () => {
   const { socket } = useSocketContext();
-  const { messages, setMessages } = useConversation();
-
+  const { selectedConversation,messages, setMessages } = useConversation();
+  const [senderIdtemp,setSenderIdtemp]  = useState("");
   useEffect(() => {
     socket?.on("newMessage", (newMessage) => {
+      // console.log(newMessage);
+      // console.log(selectedConversation);
+
       newMessage.shouldShake = true;
       const sound = new Audio(notificationSound);
       sound.play();
-      setMessages([...messages, newMessage]);
+      setSenderIdtemp(newMessage.senderId);
+      if(selectedConversation._id === newMessage.senderId) setMessages([...messages, newMessage]);
     });
-
+    if(selectedConversation._id !== senderIdtemp) return () => socket?.off("newMessage");
     return () => socket?.off("newMessage");
   }, [socket,setMessages,messages]);
 };
